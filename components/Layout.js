6d95ba74677a41bf5590ff9895cee26941b7b7da
Angkon Kumar Roy <45168734+akr5515@users.noreply.github.com>
2021-12-15 12:15:05 +0600

    Add files via upload

---
+++ components/Layout.js
@@ -0,0 +1,90 @@
+import Head from "next/head";
+import {
+  AppBar,
+  Container,
+  Link,
+  Toolbar,
+  Typography,
+  ThemeProvider,
+  Switch,
+  CssBaseline,
+} from "@material-ui/core";
+import { createTheme } from "@material-ui/core/styles";
+import useStyles from "../utilis/styles";
+import NextLink from "next/link";
+import { useContext } from "react";
+import { Store } from "../utilis/store";
+import Cookies from "js-cookie";
+
+export default function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const darkMode = state;
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+    },
+    palette: {
+      mode: darkMode ? "dark" : "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
+  const classes = useStyles();
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
+    const newDarkMode = !darkMode;
+    Cookies.set("darkMode", newDarkMode ? "ON" : "OFF");
+  };
+  return (
+    <div>
+      <Head>
+        <title>
+          {title ? `${title} - NextJS Commerce` : "NextJS Commerce"}
+        </title>
+        {description && <meta name="description" content={description} />}
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>NextJS</Typography>
+              </Link>
+            </NextLink>
+            <div className={classes.grow}></div>
+            <div>
+              <Switch
+                checked={darkMode}
+                onClick={darkModeChangeHandler}
+              ></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All rights reserved</Typography>
+        </footer>
+      </ThemeProvider>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+  CardActions,
+  Button,
+} from "@material-ui/core";
+import Layout from "../components/Layout";
+import data from "../utilis/data";
+import NextLink from "next/link";
+
+export default function Home() {
+  return (
+    <Layout>
+      <div>
+        <h1>Products</h1>
+        <Grid container spacing={3}>
+          {data.products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <NextLink href={`/product/${product.slug}`} passHref>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      title={product.name}
+                    />
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </NextLink>
+                <CardActions>
+                  <Typography>${product.price}</Typography>
+                  <Button size="small" color="primary">
+                    Add to Cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </Layout>
+  );
+}
